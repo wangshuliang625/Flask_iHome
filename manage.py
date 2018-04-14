5790d362d2b75e5b7a2ab676993941bc50a79f9d
smartlz <1009098051@qq.com>
2018-04-14 17:16:58 +0800

    数据库迁移集成

--- manage.py
+++ manage.py
@@ -4,6 +4,8 @@ from flask import Flask, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
 from flask_session import Session
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 
 
 class Config(object):
@@ -50,6 +52,12 @@ CSRFProtect(app)
 # session存储
 Session(app)
 
+# 创建Manager对象
+manager = Manager(app)
+Migrate(app, db)
+# 添加数据库迁移命令
+manager.add_command('db', MigrateCommand)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -62,4 +70,5 @@ def index():
 
 if __name__ == '__main__':
     # 运行开发web服务器
-    app.run()
+    # app.run()
+    manager.run()
