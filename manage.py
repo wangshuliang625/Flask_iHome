59f0553f673d6e5e6e703c740af3df8a90ccf464
smartlz <1009098051@qq.com>
2018-04-14 17:12:02 +0800

    flask_session集成

--- manage.py
+++ manage.py
@@ -1,8 +1,9 @@
 # coding=utf-8
 import redis
-from flask import Flask
+from flask import Flask, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
+from flask_session import Session
 
 
 class Config(object):
@@ -21,6 +22,16 @@ class Config(object):
     REDIS_HOST = '172.16.179.139'
     REDIS_PORT = 6379
 
+    # session存储设置
+    # 设置session存储到redis中
+    SESSION_TYPE = 'redis'
+    # 设置存储session的redis的地址
+    SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
+    # 设置session信息加密
+    SESSION_USE_SIGNER = True
+    # 设置session的过期时间
+    PERMANENT_SESSION_LIFETIME = 86400 * 2
+
 # 创建Flask应用程序实例
 app = Flask(__name__)
 
@@ -36,12 +47,17 @@ redis_store = redis.StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
 # 开启csrf保护
 CSRFProtect(app)
 
+# session存储
+Session(app)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     # 测试redis
     # 因为这里是测试代码，所以注释掉
     # redis_store.set('name', 'laowang')
+    # 测试session
+    # session['name'] = 'smart'
     return 'index'
 
 if __name__ == '__main__':
