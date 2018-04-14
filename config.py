e62858e3541b3c8e46d61e507c6058136e359cdf
smartlz <1009098051@qq.com>
2018-04-14 17:32:46 +0800

    抽取配置类到单独文件

---
+++ config.py
@@ -0,0 +1,29 @@
+# coding=utf-8
+import redis
+
+
+class Config(object):
+    """配置类"""
+    DEBUG = True
+
+    # 设置SECRET_KEY
+    SECRET_KEY = 'qyEzGidVnaRZNInFA6lO7AoPgIJGr83Em+wXttn8rBEGnbRswiviq5moyKDXG21j'
+
+    # 数据库配置
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@172.16.179.139:3306/ihome26'
+    # 关闭追踪数据库的修改
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # redis配置
+    REDIS_HOST = '172.16.179.139'
+    REDIS_PORT = 6379
+
+    # session存储设置
+    # 设置session存储到redis中
+    SESSION_TYPE = 'redis'
+    # 设置存储session的redis的地址
+    SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
+    # 设置session信息加密
+    SESSION_USE_SIGNER = True
+    # 设置session的过期时间
+    PERMANENT_SESSION_LIFETIME = 86400 * 2
--- manage.py
+++ manage.py
@@ -4,35 +4,12 @@ from flask import Flask, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
 from flask_session import Session
-from flask_script import Manager
-from flask_migrate import Migrate, MigrateCommand
 
+from config import Config
 
-class Config(object):
-    """配置类"""
-    DEBUG = True
+# from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand, Manager
 
-    # 设置SECRET_KEY
-    SECRET_KEY = 'qyEzGidVnaRZNInFA6lO7AoPgIJGr83Em+wXttn8rBEGnbRswiviq5moyKDXG21j'
-
-    # 数据库配置
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@172.16.179.139:3306/ihome26'
-    # 关闭追踪数据库的修改
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    # redis配置
-    REDIS_HOST = '172.16.179.139'
-    REDIS_PORT = 6379
-
-    # session存储设置
-    # 设置session存储到redis中
-    SESSION_TYPE = 'redis'
-    # 设置存储session的redis的地址
-    SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
-    # 设置session信息加密
-    SESSION_USE_SIGNER = True
-    # 设置session的过期时间
-    PERMANENT_SESSION_LIFETIME = 86400 * 2
 
 # 创建Flask应用程序实例
 app = Flask(__name__)
