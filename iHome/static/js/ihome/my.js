5d018370907b64b293fc99e7ebf97550135029c6
smartlz <1009098051@qq.com>
2018-04-19 17:23:47 +0800

    (我的爱家)个人中心加载用户的个人信息

--- iHome/static/js/ihome/my.js
+++ iHome/static/js/ihome/my.js
@@ -11,5 +11,18 @@ function logout() {
 $(document).ready(function(){
 
     // TODO: 在页面加载完毕之后去加载个人信息
+    $.get('/api/v1.0/user', function (resp) {
+        if (resp.errno == '0') {
+            // 获取信息成功
+            // 设置用户的头像地址，用户名和手机号
+            $('#user-avatar').attr('src', resp.data.avatar_url);
+            $('#user-name').html(resp.data.username);
+            $('#user-mobile').html(resp.data.mobile);
+        }
+        else {
+            // 获取信息失败
+            alert(resp.errmsg);
+        }
+    })
 
 });
