dc56e64300c4825089a1b8b7efaa099608a2b96a
smartlz <1009098051@qq.com>
2018-04-20 16:22:22 +0800

    发布房屋的基本信息后端逻辑&前端逻辑

--- iHome/api_1_0/house.py
+++ iHome/api_1_0/house.py
@@ -2,10 +2,90 @@
 # 此文件定义和房屋有关的api
 from . import api
 
+from iHome import db
 from iHome.models import Area
 from iHome.response_code import RET
+from iHome.models import House, Facility
+from iHome.utils.commons import login_required
 
-from flask import current_app, jsonify
+from flask import current_app, jsonify, request, g
+
+
+@api.route('/houses', methods=["POST"])
+@login_required
+def save_new_house():
+    """
+    发布新房源:
+    1. 接收房屋的基本信息并进行参数校验
+    2. 创建House对象并保存房屋基本信息
+    3. 将房屋的基本信息添加进数据库
+    4. 返回应答
+    """
+    # 1. 接收房屋的基本信息并进行参数校验
+    req_dict = request.json
+
+    title = req_dict.get('title')
+    price = req_dict.get('price') # 房屋价格
+    address = req_dict.get('address')
+    area_id = req_dict.get('area_id')
+    room_count = req_dict.get('room_count')
+    acreage = req_dict.get('acreage')
+    unit = req_dict.get('unit')
+    capacity = req_dict.get('capacity')
+    beds = req_dict.get('beds')
+    deposit = req_dict.get('deposit') # 房屋押金
+    min_days = req_dict.get('min_days')
+    max_days = req_dict.get('max_days')
+
+    if not all([title, price, address, area_id, room_count, acreage, unit, capacity, beds, deposit, min_days, max_days]):
+        return jsonify(errno=RET.PARAMERR, errmsg='参数缺失')
+
+    try:
+        # 数据库中房屋的价格和押金以 分 保存
+        price = float(price) * 100
+        deposit = float(deposit) * 100
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.PARAMERR, errmsg='参数错误')
+
+    # 2. 创建House对象并保存房屋基本信息
+    house = House()
+    house.user_id = g.user_id
+    house.area_id = area_id
+    house.title = title
+    house.price = price
+    house.address = address
+    house.room_count = room_count
+    house.acreage = acreage
+    house.unit = unit
+    house.capacity = capacity
+    house.beds = beds
+    house.deposit = deposit
+    house.min_days = min_days
+    house.max_days = max_days
+
+    # 获取房屋的设施信息
+    facility = req_dict.get('facility') # [1, 3, 4]
+
+    try:
+        facilities = Facility.query.filter(Facility.id.in_(facility)).all()
+        if facilities:
+            house.facilities = facilities
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg='获取房屋设施信息失败')
+
+    # 3. 将房屋的基本信息添加进数据库
+    # try:
+    #     db.session.add(house)
+    #     db.session.commit()
+    # except Exception as e:
+    #     db.session.rollback()
+    #     current_app.logger.error(e)
+    #     return jsonify(errno=RET.DBERR, errmsg='保存房屋信息失败')
+
+    # 4. 返回应答
+    return jsonify(errno=RET.OK, errmsg='OK')
 
 
 @api.route('/areas')
--- iHome/static/js/ihome/newhouse.js
+++ iHome/static/js/ihome/newhouse.js
@@ -14,10 +14,16 @@ $(document).ready(function(){
             // 遍历设置所有城区的信息
             var areas = resp.data;
 
+            /*
             for (var i=0; i<areas.length; i++) {
+                // 获取每个城区信息
                 var area = areas[i];
+                // 向城区下拉列表框中添加option选择项
                 $("#area-id").append('<option value="' + area.aid + '">' + area.aname + '</option>')
-            }
+            }*/
+
+            var html = template("areas-tmpl", {"areas": areas});
+            $("#area-id").append(html);
         }
         else {
             // 出错
@@ -26,6 +32,51 @@ $(document).ready(function(){
     })
 
     // TODO: 处理房屋基本信息提交的表单数据
+    $("#form-house-info").submit(function (e) {
+        e.preventDefault();
+
+        // 获取房屋的基本信息
+        var house_params = {};
+        $(this).serializeArray().map(function (x) {
+            house_params[x.name] = x.value;
+        })
+
+        // 获取被选中的房屋设施的id
+        var facility = [];
+        $(":checked[name=facility]").each(function (index, item) {
+            facility[index] = item.value;
+        })
+
+        house_params["facility"] = facility;
+
+        // 请求发布房屋信息
+        $.ajax({
+            "url": "/api/v1.0/houses",
+            "type": "post",
+            "data": JSON.stringify(house_params),
+            "contentType": "application/json",
+            "headers": {
+                "X-CSRFToken": getCookie("csrf_token")
+            },
+            "success": function (resp) {
+                if (resp.errno == "0") {
+                    // 发布房屋信息成功
+                    // 隐藏房屋基本信息表单
+                    $("#form-house-info").hide();
+                    // 显示上传房屋图片的表单
+                    $("#form-house-image").show();
+                }
+                else if (resp.errno == "4101") {
+                    // 未登录
+                    location.href = "login.html";
+                }
+                else {
+                    // 出错
+                    alert(resp.errmsg);
+                }
+            }
+        })
+    })
 
     // TODO: 处理图片表单的数据
 
